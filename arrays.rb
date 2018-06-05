969af1d2a2441d0441410b57428c29db53f39f9b
valeria farfan <valeria073104@gmail.com>
2018-06-05 21:46:34 +0000

    automatically backed up by learn

--- arrays.rb
+++ arrays.rb
@@ -16,3 +16,7 @@ puts student_names
 #array name ('what you want to add').push will add it to the array
 student_names.push('Vanessa', 'Valentina', 'Veronica')
 puts student_names
+
+
+student_names << "mac"
+puts student_names
