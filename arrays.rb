ea0f064c3bd67c37895e65f43f440f4b13e9b02e
valeria farfan <valeria073104@gmail.com>
2018-06-05 21:36:35 +0000

    automatically backed up by learn

---
+++ arrays.rb
@@ -0,0 +1,7 @@
+student_names=[]
+#arrays are formed by usingsquare brackets [ ]
+
+student_names = ['sam','jess','ash','bob','carla']
+puts student_names
+
+#puts [name of your array] prints out the information in your array in the terrminal 
